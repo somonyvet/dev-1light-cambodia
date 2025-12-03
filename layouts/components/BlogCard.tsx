59c4617e3429515b9e04fd8c23a02cb401b6fc71
lihong <lihonghun135@gmail.com>
2025-12-04 00:17:30 +0700

    --translate blog page

--- layouts/components/BlogCard.tsx
+++ layouts/components/BlogCard.tsx
@@ -1,4 +1,4 @@
-import {FC, useEffect, useState} from "react";
+import {FC, useState} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import {useRouter} from "next/navigation";
